86f1b30539a42557c2011a58d61866aaef1f57c2
shuting <154670798+shuting52@users.noreply.github.com>
2026-09-25 01:23:20 +0800

    feat v1.7.9/v2.0.2: build.gradle.kts

--- console-apk/app/build.gradle.kts
+++ console-apk/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.yuntai"
         minSdk = 24
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.0.1"
+        versionCode = 22
+        versionName = "2.0.2"
     }
 
     signingConfigs {
